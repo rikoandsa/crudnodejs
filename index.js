5ef2c2d5eff7481fb67316e507ea6f8a1b19b3dd
riku-and-sa <andri.riku@gmail.com>
2020-07-28 12:53:54 +0700

    inisialisasi modul

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ const path = require("path");
 const express = require("express");
 const mysql = require("mysql");
 const bodyParser = require("body-parser");
+const app = express();
+// insialisasi engine hbs
 const hbs = require("hbs");
 
-const app = express();
 
+// menjalankan server di port 4000/github.com/riku-and-sa/crudnodejs.git
+   a532b73..fac7e2e  master -> master
+   riko@riko-X450CA:~/learn/crudnodejs
 app.listen(4000, () => {
   console.log("Server berjalan di 127.0.0.1:4000");
 });
+
+
